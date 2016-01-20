e06179036933596387566d5e262d28b26be97436
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-20 19:42:48 +0000

    seed database with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  
+  p1 = Post.create(title: "Man Jumps 25 feet in the air!", 
+  body: "Tootsie roll cake sweet cotton candy apple pie icing. Marzipan sesame snaps carrot cake muffin chocolate cake ice cream. 
+  Soufflé candy lollipop cupcake chocolate cake donut pudding soufflé. Chocolate cake ice cream bear claw wafer sugar plum. Oat cake powder pastry gingerbread jujubes. 
+  Oat cake lemon drops lollipop macaroon donut gummies jelly ice cream oat cake. 
+  Candy canes fruitcake brownie apple pie topping pudding. Candy canes sesame snaps bear claw candy liquorice powder.")
+  
+  p2 = Post.create(title: "A crisp frosty morning", body: "Tootsie roll cake sweet cotton candy apple pie icing. Marzipan sesame snaps carrot cake muffin chocolate cake ice cream. 
+  Soufflé candy lollipop cupcake chocolate cake donut pudding soufflé. Chocolate cake ice cream bear claw wafer sugar plum. Oat cake powder pastry gingerbread jujubes. 
+  Oat cake lemon drops lollipop macaroon donut gummies jelly ice cream oat cake. Candy canes fruitcake brownie apple pie topping pudding. Candy canes sesame snaps bear 
+  claw candy liquorice powder.")
+  
+  p3 = Post.create(title: "The perfect pet is...", body: "Tootsie roll cake sweet cotton candy apple pie icing. Marzipan sesame snaps carrot cake muffin chocolate cake ice cream. 
+  Soufflé candy lollipop cupcake chocolate cake donut pudding soufflé. Chocolate cake ice cream bear claw wafer sugar plum. Oat cake powder pastry gingerbread jujubes. 
+  Oat cake lemon drops lollipop macaroon donut gummies jelly ice cream oat cake. Candy canes fruitcake brownie apple pie topping pudding. Candy canes sesame snaps bear 
+  claw candy liquorice powder.")
+  
+  p4 = Post.create(title: "Recycling, the way of the future", body: "Tootsie roll cake sweet cotton candy apple pie icing. Marzipan sesame snaps carrot cake muffin chocolate cake ice cream. 
+  Soufflé candy lollipop cupcake chocolate cake donut pudding soufflé. Chocolate cake ice cream bear claw wafer sugar plum. Oat cake powder pastry gingerbread jujubes. 
+  Oat cake lemon drops lollipop macaroon donut gummies jelly ice cream oat cake. Candy canes fruitcake brownie apple pie topping pudding. Candy canes sesame snaps bear 
+  claw candy liquorice powder.")
+  
+  p5 = Post.create(title: "A song stuck in your head", body: "Tootsie roll cake sweet cotton candy apple pie icing. Marzipan sesame snaps carrot cake muffin chocolate cake ice cream. 
+  Soufflé candy lollipop cupcake chocolate cake donut pudding soufflé. Chocolate cake ice cream bear claw wafer sugar plum. Oat cake powder pastry gingerbread jujubes. 
+  Oat cake lemon drops lollipop macaroon donut gummies jelly ice cream oat cake. Candy canes fruitcake brownie apple pie topping pudding. Candy canes sesame snaps bear 
+  claw candy liquorice powder.")
