c9842d65220b0c8c48b6afa56b4f78ab9ff0c536
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-20 20:37:36 +0000

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   resources :posts, only: [:index, :show]
