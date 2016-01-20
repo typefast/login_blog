96c72185b73ae82b223cc36ee77498a263b58b47
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-20 19:32:32 +0000

    Fill out index and show actions, add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   get 'posts/show'
+  
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
