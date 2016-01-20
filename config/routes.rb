3be52455af049ac0cfe899eeb2cb139b0827714a
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-20 19:46:50 +0000

    Update routes, fix show action typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/show'
-  
+  resources :posts, only: [:index, :show]
   root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
